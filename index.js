e8df9b19c523ddb9f7830a79c4f8a776d15e6e22
Cormac <cormac_gudge@hotmail.com>
2020-10-05 14:56:20 +0100

    testing

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const { spawn } = require('child_process');
 
-// const cmd = "wineconsole --backend=curses '/home/cormac/Mount&Blade Warband Napoleonic Wars Dedicated/mb_warband_dedicated.exe' -r NW_Sample_Duel.txt";
-// exec(cmd, (error, stdout, stderr) => {
-//   if (error) {
-//     console.log(`error: ${error.message}`);
-//     return;
-//   }
-//   if (stderr) {
-//     console.log(`stderr: ${stderr}`);
-//     return;
-//   }
-//   console.log(`stdout: ${stdout}`);
+const cmd = "wineconsole --backend=curses '/home/cormac/Mount&Blade Warband Napoleonic Wars Dedicated/mb_warband_dedicated.exe' -r NW_Sample_Duel.txt";
+exec(cmd, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+// const ls = spawn("wineconsole", ["--backend=curses '/home/cormac/Mount&Blade Warband Napoleonic Wars Dedicated/mb_warband_dedicated.exe' -r NW_Sample_Duel.txt"]);
+
+// ls.stdout.on("data", data => {
+//     console.log(`stdout: ${data}`);
 // });
 
-const ls = spawn("wineconsole", ["--backend=curses '/home/cormac/Mount&Blade Warband Napoleonic Wars Dedicated/mb_warband_dedicated.exe' -r NW_Sample_Duel.txt"]);
+// ls.stderr.on("data", data => {
+//     console.log(`stderr: ${data}`);
+// });
 
-ls.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-});
+// ls.on('error', (error) => {
+//     console.log(`error: ${error.message}`);
+// });
 
-ls.stderr.on("data", data => {
-    console.log(`stderr: ${data}`);
-});
-
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-});
-
-ls.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-});
+// ls.on("close", code => {
+//     console.log(`child process exited with code ${code}`);
+// });
