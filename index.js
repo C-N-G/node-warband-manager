85f41e1cccd6f03c3bd5094e8c593dd7968025a2
Cormac <cormac_gudge@hotmail.com>
2020-10-05 15:41:21 +0100

    testing

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const { exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 
-const cmd = "wineconsole --backend=curses /home/cormac/nw_server/mb_warband_dedicated.exe -r NW_Sample_Duel.txt -m Napoleonic Wars";
-exec(cmd, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+// const cmd = "wineconsole --backend=curses /home/cormac/nw_server/mb_warband_dedicated.exe -r NW_Sample_Duel.txt -m Napoleonic Wars";
+// exec(cmd, (error, stdout, stderr) => {
+//   if (error) {
+//     console.log(`error: ${error.message}`);
+//     return;
+//   }
+//   if (stderr) {
+//     console.log(`stderr: ${stderr}`);
+//     return;
+//   }
+//   console.log(`stdout: ${stdout}`);
+// });
 
 // set_map mp_french_farm
 // scn_mp_french_farm.sco  
 
-// const ls = spawn("wineconsole", ["--backend=curses '/home/cormac/Mount&Blade Warband Napoleonic Wars Dedicated/mb_warband_dedicated.exe' -r NW_Sample_Duel.txt"]);
+const ls = spawn("wineconsole", ["--backend=curses /home/cormac/nw_server/mb_warband_dedicated.exe", "-r NW_Sample_Duel.txt", "-m Napoleonic Wars"]);
 
-// ls.stdout.on("data", data => {
-//     console.log(`stdout: ${data}`);
-// });
+ls.stdout.on("data", data => {
+    console.log(`stdout: ${data}`);
+});
 
-// ls.stderr.on("data", data => {
-//     console.log(`stderr: ${data}`);
-// });
+ls.stderr.on("data", data => {
+    console.log(`stderr: ${data}`);
+});
 
-// ls.on('error', (error) => {
-//     console.log(`error: ${error.message}`);
-// });
+ls.on('error', (error) => {
+    console.log(`error: ${error.message}`);
+});
 
-// ls.on("close", code => {
-//     console.log(`child process exited with code ${code}`);
-// });
+ls.on("close", code => {
+    console.log(`child process exited with code ${code}`);
+});
  
 
